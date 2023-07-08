8f04e4edf659d17fbf4146f76904815693b8b63e
Roshin Prasad <88421254+RoshinPrasad@users.noreply.github.com>
2023-07-08 12:24:56 +0530

    Update Axios.js

--- frontend/src/Axios.js
+++ frontend/src/Axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'http://localhost:',
   headers: {
     'Content-Type': 'application/json',
   }
